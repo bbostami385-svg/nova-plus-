7a5e8b76bfcfa37d401a3df09f0cb514a9239c9d
Bayojid AI <bbostami385@gmail.com>
2026-03-09 07:31:13 +0600

    Add Feed component for social feed display
    Signed-off-by: Bayojid AI  <bbostami385@gmail.com>

---
+++ Feed.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Feed() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>NovaPlus Social Feed 🚀</h1>
+      <p>All posts will appear here</p>
+    </div>
+  );
+}
+
+export default Feed;
