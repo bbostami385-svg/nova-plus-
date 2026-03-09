da0655e2ce6f17c0e48ae693f5b5cfdf8319a8c4
Bayojid AI <bbostami385@gmail.com>
2026-03-09 10:28:23 +0600

    Refactor Signup component and remove name input
    Removed name input and modified handleSignup function to log email and password. Updated styling and structure of the Signup component.
    
    Signed-off-by: Bayojid AI  <bbostami385@gmail.com>

--- Signup.js
+++ Signup.js
@@ -1,45 +1,21 @@
 import React, { useState } from "react";
-import LoginLogo from "../assets/login_logo.png";
+import LoginLogo from "../assets/login_logo.png"; // same logo can be used
 
 function Signup() {
-  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSignup = async () => {
-    try {
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ name, email, password }),
-      });
-      const data = await res.json();
-      alert(data.msg);
-    } catch (err) {
-      console.error(err);
-      alert("Server error");
-    }
+  const handleSignup = () => {
+    console.log("Signup clicked", email, password);
+    // TODO: Firebase Auth integration
   };
 
   return (
-    <div style={{ textAlign: "center" }}>
-      <img src={LoginLogo} alt="Signup Logo" width={150} height={150} />
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <img src={LoginLogo} alt="Signup Logo" style={{ width: "150px" }} />
       <h2>Signup</h2>
-      <input
-        type="text"
-        placeholder="Name"
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
+      <input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
       <button onClick={handleSignup}>Signup</button>
     </div>
   );
