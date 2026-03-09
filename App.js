72f0376c35a6bd1cbd757914fd5cd00c03c2979b
Bayojid AI <bbostami385@gmail.com>
2026-03-09 06:53:15 +0600

    Replace Feed route with Splash route
    Signed-off-by: Bayojid AI  <bbostami385@gmail.com>

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Feed from "./pages/index";
 import Chat from "./pages/chat";
+import Splash from "./pages/Splash";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Splash />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/feed" element={<Feed />} />
         <Route path="/chat" element={<Chat />} />
       </Routes>
     </Router>
